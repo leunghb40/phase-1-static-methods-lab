048ed56c5dc359cf7033e09d1462f860a557f0c4
Qing Bin Liang <leunghb@hotmail.com>
2023-04-09 23:34:42 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 class Formatter {
-  //add static methods here
+  static capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1)
+  }
+
+  static sanitize(str) {
+    return str.replace(/[^A-Za-z0-9-' ]/g, '')
+  }
+
+  static titleize(str) {
+    const exceptions = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    return str.split(' ').map((word, index) => {
+      if (index === 0 || !exceptions.includes(word)) {
+        return this.capitalize(word)
+      } else {
+        return word
+      }
+    }).join(' ')
+  }
 }
